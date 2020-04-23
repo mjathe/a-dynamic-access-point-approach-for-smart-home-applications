85ab334f09720a3ee2ffc137ecc15d955373b774
mjathe <mjathe@users.noreply.github.com>
2020-04-23 10:49:52 +0200

    .

---
+++ mosquitto/mosquitto_3_broker.bat
@@ -0,0 +1,4 @@
+TITLE 3MosquittoBroker
+START mosquitto -p 1883
+START mosquitto -p 1884
+START mosquitto -p 1882
---
+++ python_client/mqtt_client2.py
@@ -0,0 +1,34 @@
+import paho.mqtt.client as mqtt
+import subprocess
+
+server = False
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("IPTABLE")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    if server == True:
+        iptable[msg.payload] =msg.payload
+        print(iptable)
+brokerport = str(1883)
+broker = subprocess.Popen("D:\Sciebo\STUDIUM\Bachelorarbeit\mosquitto\mosquitto -p "+brokerport, shell=False)
+print("Broker auf port "+brokerport+" gestartet.")
+iptable ={}
+
+client = mqtt.Client(client_id="1883")
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1882, 60)
+client.publish("IPTABLE", brokerport, qos=0, retain=False)
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
